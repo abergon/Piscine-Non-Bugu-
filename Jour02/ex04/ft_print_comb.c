3105d1db943822a53c8d1baf92c6f1987cc3346e
Adrian BERGON <abergon@e1r12p3.42.fr>
2017-08-07 10:50:42 +0200

    cbonclerousse

--- Jour02/ex04/ft_print_comb.c
+++ Jour02/ex04/ft_print_comb.c
@@ -6,30 +6,28 @@
 /*   By: abergon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/06 02:49:28 by abergon           #+#    #+#             */
-/*   Updated: 2017/08/06 17:31:29 by abergon          ###   ########.fr       */
+/*   Updated: 2017/08/07 10:48:10 by abergon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int			ft_putchar(char c);
+int		ft_putchar(char c);
 
-void		ft_realone(int a, int b, int c)
+void	ft_realone(int a, int b, int c)
 {
 	ft_putchar(a + 48);
 	ft_putchar(b + 48);
 	ft_putchar(c + 48);
 }
 
-void		ft_print_comb(void)
+void	ft_print_comb(void)
 {
 	int a;
 	int b;
 	int c;
 
 	a = 0;
-	b = 0;
-	c = 0;
 	while (a <= 7)
 	{
 		b = a + 1;
