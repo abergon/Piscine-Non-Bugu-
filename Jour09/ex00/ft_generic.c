0a66e7db22cc54759c315cef323f0ec9019e5a20
Adrian BERGON <abergon@e1r12p3.42.fr>
2017-08-10 16:58:27 +0200

    j09ex00opti

--- Jour09/ex00/ft_generic.c
+++ Jour09/ex00/ft_generic.c
@@ -6,13 +6,11 @@
 /*   By: abergon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 16:49:00 by abergon           #+#    #+#             */
-/*   Updated: 2017/08/10 16:53:47 by abergon          ###   ########.fr       */
+/*   Updated: 2017/08/10 16:57:49 by abergon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void		ft_generic(void)
-{
 	write(1, "Tut tut ; Tut tut\n", 18);
-}
