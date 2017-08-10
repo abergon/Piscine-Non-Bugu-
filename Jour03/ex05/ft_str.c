1d86fe53bf2dd162b5c9bbcb5b1748c5754b2351
Adrian BERGON <abergon@e1r12p3.42.fr>
2017-08-10 16:54:46 +0200

    d09ex00

--- Jour03/ex05/ft_str.c
+++ Jour03/ex05/ft_str.c
@@ -1,17 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_str.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abergon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/08 10:56:13 by abergon           #+#    #+#             */
-/*   Updated: 2017/08/09 11:22:00 by abergon          ###   ########.fr       */
+/*   Created: 2017/08/09 16:49:41 by abergon           #+#    #+#             */
+/*   Updated: 2017/08/10 16:30:51 by abergon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+#include <unistd.h>
+
+int		ft_putchar(char str)
 {
-	*div = a / b;
-	*mod = a % b;
+	write(1, &str, 1);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	str[5] = "truck"
+	ft_putchar(str);
+}
+
+int		main()
+{
+	ft_putstr(str);
+	return (0);
 }
